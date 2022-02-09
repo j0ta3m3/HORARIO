0c42d871f2eae8daccbad04a2ec37f78f7c30ede
ManuelJisus <91907544+ManuelJisus@users.noreply.github.com>
2022-02-09 19:22:19 +0100

    Update Entrada.kt

--- src/main/kotlin/Entrada.kt
+++ src/main/kotlin/Entrada.kt
@@ -4,10 +4,18 @@
  * @constructor hay dos variables. La primera lee la cabecera del horario, la segunda lee los datos para cada celda del horario
  */
 
+open class PedirPantalla: Horario() {
+    var cabecera : Cabecera = Cabecera()
+    open   fun pedircabecera() : List<String> {
+        println("Porfavor introduzca la cabecera del horario separando los elementos por ;")
+        return  cabecera.formateo(readln())
 
-class Entrada {
 
-    var elementosCabecera = readlnOrNull()
-    var elementosLinea = readlnOrNull()
+        println("Porfavor introduzca una linea del horario separando los elementos por ; para terminar introduzca un valor en blanco")
+
+
+
+
+    }
 
 }
