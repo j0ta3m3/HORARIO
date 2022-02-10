97d195b0386a96535711696da85a479ff5acc3ae
Elian64 <edeniz@iesrafaelalberti.es>
2022-02-10 13:53:01 +0100

    posbile bucle para lista

--- src/main/kotlin/PROYECTO_BUENO/Main.kt
+++ src/main/kotlin/PROYECTO_BUENO/Main.kt
@@ -1,13 +1,32 @@
 package PROYECTO_BUENO
 
-fun main(){
+fun main() {
 
-    var horario1 = Horario()
+    /*var horario1 = Horario()
 
     println("Introduzca un horario")
     var entrada = readLine().toString()
-    println( Horario().formateo(entrada))
+    Horario().formateo(entrada)*/
 
 
+    /* var confirm=false
+
+        var horario1 = Horario()
+
+    while(confirm!=true) {
+        for(i in 0..Horario().Horario.size) {
+            println("Introduzca un horario")
+            var entrada = readLine().toString()
+            var hola:List<String> = Horario().formateo(entrada)
+            Horario().Horario[i]= hola[i]
+            //Horario().Horario.size==
+            if (entrada.isEmpty()) {
+                confirm = true
+
+            }
+        }
+    }
+
+    println(Horario().Horario.toString())*/
 
 }
