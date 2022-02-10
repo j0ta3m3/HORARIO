80de63de92992d061b789801b923b32967a8429c
speedmatao <73549446+speedmatao@users.noreply.github.com>
2022-02-10 18:33:59 +0100

    suu

--- src/main/kotlin/Horario.kt
+++ src/main/kotlin/Horario.kt
@@ -4,8 +4,8 @@ open  class Horario {
 
     var Horario = mutableListOf<String>()
 
-    fun añadir ( añade : List<String> ){
-        Horario.addAll(añade)
+    fun añadir ( añade : String ){
+        Horario.add(añade)
     }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    var pedir = PedirporPantalla()
+    pedir.dameHorario()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
 }
