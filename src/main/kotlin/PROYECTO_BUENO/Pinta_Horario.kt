fc1721ad3ca9b1c0a9ae7c44f652edf169869a87
jose m <josemarisanchezjimenez@gmail.com>
2022-02-17 19:48:06 +0100

    bucle for para pintar las filas em la clase PintaHorario()

--- src/main/kotlin/PROYECTO_BUENO/Pinta_Horario.kt
+++ src/main/kotlin/PROYECTO_BUENO/Pinta_Horario.kt
@@ -4,21 +4,21 @@ import de.m3y.kformat.Table
 import de.m3y.kformat.table
 
 
-class PintaHorario(){
+class PintaHorario() {
 
-fun tablita(listita: Horario) {
-    var h = table {
-        header(listita.Horario[0])
+    fun tablita(listita: Horario) {
+        var h = table {
+            header(listita.Horario[0])
+            listita.Horario.removeAt(0)
+            for (i in listita.Horario.indices) {
+                row(listita.Horario[i])
+            }
 
-        for(i in 0..listita.Horario.size-1)
-        row(listita.Horario[i])
-
-        hints {
-            borderStyle = Table.BorderStyle.SINGLE_LINE
-        }
-
-    }.render(StringBuilder())
-println(h)
-}
+            hints {
+                borderStyle = Table.BorderStyle.SINGLE_LINE
+            }
+        }.render(StringBuilder())
+        println(h)
+    }
 
 }
